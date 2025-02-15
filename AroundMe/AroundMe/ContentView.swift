0c46a077f9e6478de05f80f605cd0d7da9fbaf02
Kevin Perez <62121543+kperez755@users.noreply.github.com>
2025-02-15 10:22:50 -0500

    Add files via upload

---
+++ AroundMe/AroundMe/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  AroundMe
+//
+//  Created by Kevin Perez on 2/11/25.
+//
+
+import SwiftUI
+import MapKit
+
+
+struct ContentView: View {
+    
+    @State private var query: String = ""
+    
+    @State private var selectedDetent: PresentationDetent = .fraction(0.15)
+    @State private var locationManager = LocationManager.shared
+    
+    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    @State private var isSearching: Bool = false
+    
+    @State private var mapItems: [MKMapItem] = []
+    @State private var visibleRegion: MKCoordinateRegion?
+    
+    private func search() async {
+        do {
+            mapItems = try await performSearch(searchTerm: query, visibleRegion: visibleRegion)
+            print(mapItems)
+            isSearching = false
+        } catch {
+            mapItems = []
+            print(error.localizedDescription)
+            isSearching = false
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            Map(position: $position) {
+                ForEach(mapItems, id: \.self) { mapItem in
+                    Marker(item: mapItem)
+                }
+                UserAnnotation()
+
+            }
+            
+            .onChange(of: locationManager.region, {
+                
+            })
+                .sheet(isPresented: .constant(true), content: {
+                    VStack {
+                        TextField("Search", text: $query)
+                        
+                            .textFieldStyle(.roundedBorder)
+                            .padding()
+                            .onSubmit {
+                                isSearching = true
+                            }
+                    }
+                    .presentationDetents([.fraction(0.15), .medium, .large], selection: $selectedDetent)
+                    .presentationDragIndicator(.visible)
+                    .interactiveDismissDisabled()
+                    .presentationBackgroundInteraction(.enabled(upThrough: .medium))
+                    
+            })
+        }
+        .onMapCameraChange { context in
+            visibleRegion = context.region
+            
+        }
+        .task(id: isSearching, {
+            if isSearching {
+                await search()
+            }
+        })
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ AroundMe/AroundMe/Utilities/MapUtilities.swift
@@ -0,0 +1,24 @@
+//
+//  MapUtilities.swift
+//  AroundMe
+//
+//  Created by Kevin Perez on 2/15/25.
+//
+
+import Foundation
+import MapKit
+import SwiftUI
+
+
+func performSearch(searchTerm: String, visibleRegion: MKCoordinateRegion?) async throws -> [MKMapItem] {
+    
+    let request = MKLocalSearch.Request()
+    request.naturalLanguageQuery = searchTerm
+    request.resultTypes = .pointOfInterest
+    guard let region = visibleRegion else { return [] }
+    request.region = region
+    
+    let search = MKLocalSearch(request: request)
+    let response = try await search.start()
+    return response.mapItems
+}
